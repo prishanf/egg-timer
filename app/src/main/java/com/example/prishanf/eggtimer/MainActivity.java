f1bc446a611320f06408d90aac59bbc98d926459
Prishan Fernando <prishan@yahoo.com>
2018-01-28 17:15:02 -0800

    Link seek bar and timer view

--- app/src/main/java/com/example/prishanf/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/prishanf/eggtimer/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.prishanf.eggtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar timerSeekBar;
+    TextView countDownTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        countDownTextView = findViewById(R.id.countdownTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int min = i/60;
+                int sec = i - (min * 60);
+                String secText = String.valueOf(sec);
+                if(sec <= 9){
+                   secText = "0"+ String.valueOf(sec);
+                }
+                countDownTextView.setText(String.valueOf(min) +":"+ secText);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
     }
 }
